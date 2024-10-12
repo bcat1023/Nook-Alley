b10686769ba42e5c86e89d848de5251d9ce68ffa
drowsy <63063627+bcat1023@users.noreply.github.com>
2024-10-12 03:02:09 -0700

    More content filled in
    Nearing sooner to the launch date of this site. Soon it will be fully finished and polished

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function tab(tab) {
     if(window.innerWidth > '864') {
-        var tabs = ["flex", "flex", "block", "block"]
+        var tabs = ["flex", "flex", "flex", "flex"]
+        // desktop
     } else {
         var tabs = ["block", "block", "block", "block"]
+         // mobile
     }
 
     // everything after this point is fucked, rewrite this please... 
 
     document.getElementById(0).style.display = 'none';
     document.getElementById(1).style.display = 'none';
+    document.getElementById(2).style.display = 'none';
+    document.getElementById(3).style.display = 'none';
     // TOP: Tab Pages | Bottom: Navigation Items
     document.getElementById(`nav0`).className = 'navitem';
     document.getElementById(`nav1`).className = 'navitem';
+    document.getElementById(`nav2`).className = 'navitem';
+    document.getElementById(`nav3`).className = 'navitem';
 
     // This part is more reasonable, prob should rewrite it still?
 
