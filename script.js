e4c1dc79efe7fbf64348ee7e21e49bac3a298592
bthecat <bthecat984@gmail.com>
2024-12-03 20:03:33 -0800

    "Pages" for SEO and users

--- script.js
+++ script.js
@@ -91,3 +91,20 @@ function projectShowcase(project) {
     }
 }
 
+
+//================================================
+// To be used with Netlify Redirects:           ||
+//    nookalley.com/projects will reverse proxy ||
+//    to nookalley.com/#projects and this script||
+//    wil handle it.                            ||
+//================================================
+var url = window.location.href;
+if(url.indexOf('#' + 'projects') != -1) {
+    tab(1)
+}
+if(url.indexOf('#' + 'about') != -1) {
+    tab(2)
+}
+if(url.indexOf('#' + 'contact') != -1) {
+    tab(3)
+}
