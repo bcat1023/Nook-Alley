b90414b48bc92a751045b13816c72fd2c45196db
bthecat <bthecat984@gmail.com>
2024-12-30 14:26:25 -0800

    Added modern formats (for bandwidth reasons) Rewrote the projectSelector (fixed a major bug) Massive decrease in bandwidth usage (if on a modern device) 	10.5x less bandwidth usage!

--- script.js
+++ script.js
@@ -85,25 +85,44 @@ function tab(tabX, resize) {
     }
 }
 
-
+var currentProject;
 function projectShowcase(project) {
-    var sources = ["/videos/video1.mp4", "/videos/video2.mp4", "/videos/video3.mp4", "/videos/video1.mp4", ""]
-    var path = document.getElementById('projectVideo').attributes.getNamedItem('src');
+    var source = ['TitanNetworkPromo', 'TheIdeaGrailsPromo', "CiscoVEN401SEPromo", "FCC-IDPromo"]
+    // Source Array | Project > PromoFile
+
+    tv = document.getElementById('projectVideo') // Root <video> player
+    ch0 = document.getElementById('projectVideoSrc0') // WebM Channel
+    ch1 = document.getElementById('projectVideoSrc1') // mp4 Channel
+
+    // Select the filename from the source array with the project number
+    fileName = source[project] 
+    // Compose url's to find the filenames
+    webM = `/videos/webm/${fileName}.webm`
+    mp4 = `/videos/legacy/${fileName}.mp4`
+    
+    // Kinda weird leftover roller script
     var roller = document.getElementById(`roller${project}`);
     var activeRoller = document.getElementsByClassName('activeRoller')
-    if (path.value == sources[project]) {
-        return;
+
+    if(ch0.src.includes(source[project])) { // If video is already set to right file return 0;
+        console.debug(`Check AlreadyPlaying passed ${ch0.src} = ${webM}`)
     } else {
-        path.value = sources[project]
+        ch0.src = webM // Set the WebM channel to the webM file
+        ch1.src = mp4  // Set the mp4 channel to the mp4 file
+        tv.load() // Load the files
+        tv.play()
+        console.debug(`Check AlreadyPlaying failed, switching content ${ch0.src} != ${webM}`)
+
         try {
-            activeRoller[0].classList.remove('activeRoller');
+            activeRoller[0].classList.remove('activeRoller'); // Select element with activeRoller class and remove its class
         } catch (err) {
             console.warn(err + "\n\nThis is technically an error, but is it really? its more of a this shouldn't have run but did because im too lazy to have it not run so really its fineee ;) Its a lazy error :)")
+            // Ignore this, ill fix it someday
         }
         roller.classList = 'activeRoller projectRollup'
+        // Give the current project roller the ActiveRoller class
     }
 }
-
 
 //================================================
 // To be used with Netlify Redirects:           ||
