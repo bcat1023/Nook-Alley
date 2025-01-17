c981c4dbde50cdbd8cbcd80499b73f8fced8d114
bthecat <bthecat984@gmail.com>
2025-01-16 17:26:36 -0800

    Quality of Life Updates: Better JS performance, I have split up the script.js file into individual files based on their function. More important scripts will be loaded first, while less important will be last or even defered
    Better video support, to eliminate the need of Adobe Premiere Pro or Adobe After Effects, I had changed the CSS to place the iMac frame over in the browser. This eliminates the need for it to be added in post.
    This change will allow me to update content more rapidly, it will remove a tedious step and allow me to on the fly fix typo's and update content.
    
    More changes will be coming soon to make the site more enjoyable
    
    Soon ill finish the rest of the videos (next commit) and make the canonical URL based on the path.

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,14 @@
 // Todo: Consider not caching the mp4 files, or have some check to cache the one the browser needs
 self.addEventListener('install', function (event) {
     event.waitUntil(
-      caches.open('Nook-Alley Video Cache').then(function (cache) {
+      caches.open('Nook-Alley Cache').then(function (cache) {
         return cache.addAll([
           '/videos/webm/home.webm',
           '/videos/webm/CiscoVEN401SEPromo.webm',
           '/videos/webm/FCC-IDPromo.webm',
           '/videos/webm/TheIdeaGrailsPromo.webm',
           '/videos/webm/TitanNetworkPromo.webm',
-          '/videos/webm/video0.webm'
+          '/videos/webm/video0.webm',
 
           //'/videos/legacy/home.mp4',
           //'/videos/legacy/CiscoVEN401SEPromo.mp4',
@@ -16,6 +16,10 @@ self.addEventListener('install', function (event) {
           //'/videos/legacy/TheIdeaGrailsPromo.mp4',
           //'/videos/legacy/TitanNetworkPromo.mp4',
           //'/videos/legacy/video0.mp4'
+
+          '/js/error.js',
+          '/js/navigation.js',
+          '/js/paths.js',
         ]);
       }),
     );
